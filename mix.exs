487e84215fc2e43044f8bfc02524b85799c97c25
Franco Montenegro <franco.montenegro.ruke@gmail.com>
2021-01-17 15:57:38 -0300

    Update version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule UnofficialMercadopagoSdkElixir.MixProject do
   def project do
     [
       app: :unofficial_mercadopago_sdk_elixir,
-      version: "1.2.0",
+      version: "1.3.0",
       elixir: "~> 1.9",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
